22656bd0f8c6ef98edf2a6fd024cbd564c9b6385
Stone <mumungan@naver.com>
2022-10-19 20:46:13 +0900

    phpwebsite

--- cymbidiumdisease.php
+++
@@ -1,6 +0,0 @@
-<?php include("menunav.php");?>
-<article>
-  <h1>심비디움의 병과 농약</h1>
-  <img src="cymbidiumdisease.jpg" width="700" alt="심비디움 병과 약재" title="심비디움 병과 약재">
-</article>
-<?php include("menufoot.php");?>
