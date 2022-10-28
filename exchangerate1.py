d4980ac89fefe8f843fcfe09d829a58edb5ddc21
Stone <mumungan@naver.com>
2022-10-28 21:19:27 +0900

    citruswork

--- exchangerate1.py
+++
@@ -1,8 +0,0 @@
-from currency_converter import CurrencyConverter
-c = CurrencyConverter()
-jpykrw = int(c.convert(100,'JPY','KRW'))
-usdkrw = int(c.convert(1,'USD','KRW'))
-usdjpy = int(c.convert(1,'USD','JPY'))
-print(jpykrw)
-print(usdkrw)
-print(usdjpy)
